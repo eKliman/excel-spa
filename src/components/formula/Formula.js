602e1862fc991db17421ab7cfb7ba232bd58e6f8
eKliman <kliman.evgeniy@gmail.com>
2020-08-14 23:03:08 +0300

    Created base table logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,26 +1,46 @@
 import {SheetsComponent} from '@core/SheetsComponent';
+import {$} from '@core/dom';
 
 export class Formula extends SheetsComponent {
   static className = 'formula';
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click'],
+      listeners: ['input', 'keydown'],
+      ...options,
     });
   }
 
   toHTML() {
     return `
       <div class="formula__info">fx</div>
-      <div class="formula__input" contenteditable spellcheck="false"></div>`;
+      <div class="formula__input"
+      id="formula"contenteditable spellcheck="false"></div>`;
+  }
+
+  init() {
+    super.init();
+    this.$formula = this.$root.find('#formula');
+
+    this.$on('table:select', ($cell) => {
+      this.$formula.text($cell.text());
+    });
+
+    this.$on('table:input', (cell) => {
+      this.$formula.text(cell.text());
+    });
   }
 
   onInput(event) {
-    console.log('Formula: onInput', event.target.textContent.trim());
+    this.$emit('formula:input', $(event.target).text());
   }
 
-  onClick(event) {
-    console.log('Formula: onClick', event);
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab'];
+    if (keys.includes(event.key)) {
+      event.preventDefault();
+      this.$emit('formula:done');
+    }
   }
 }
